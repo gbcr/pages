4583aa2217eda63dd8e46e5c503394a7a00cbada
ivanthing <ivanthing@gmail.com>
2014-11-04 17:19:21 +0800

    test subscribe

--- dist/subscribe.php
+++ dist/subscribe.php
@@ -1,21 +1,21 @@
 <?php
-phpinfo();
-// include 'Mailchimp.php';
-// $apikey = '68c9d17ced127dfb0d2a02e990b302c6-us9';
-// $api = new Mailchimp($apikey);
-// $list_id = 'd852753c71';
-// $postdata = file_get_contents("php://input");
-// $request = json_decode($postdata);
+echo phpinfo();
+include 'Mailchimp.php';
+$apikey = '68c9d17ced127dfb0d2a02e990b302c6-us9';
+$api = new Mailchimp($apikey);
+$list_id = 'd852753c71';
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
 
-// $email = array(
-//     'email' => $request->email);
+$email = array(
+    'email' => $request->email);
 
-// var_dump($email);
+var_dump($email);
 
-// $merge_vars = array();
-// $email_type = 'html';
-// $double_optin = false;
-// $send_welcome = true;
+$merge_vars = array();
+$email_type = 'html';
+$double_optin = false;
+$send_welcome = true;
 
-// $subscribe = $api->lists->subscribe($list_id, $email, $merge_vars, $email_type,$double_optin, $send_welcome);
+$subscribe = $api->lists->subscribe($list_id, $email, $merge_vars, $email_type,$double_optin, $send_welcome);
 ?>
